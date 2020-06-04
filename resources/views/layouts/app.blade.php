c740a57ece8f64ef9eacd8b33582b3c548c8ea06
Your Name <you@example.com>
2020-06-04 18:58:55 +0545

    small change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -73,7 +73,7 @@
                                 @endif
                             @else
                                 <li class="dropdown">
-                                    <a id="navbarDropdown" class="nav-link dropdown-toggle text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle text-dark" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                         {{ Auth::user()->name }} <span class="caret"></span>
                                     </a>
     
