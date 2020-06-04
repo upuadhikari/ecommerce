582aa795996c9d42f73bd45b8343e0abb5a86136
upuadhikari <upuadhikari@gmail.com>
2020-06-04 23:31:30 +0545

    all resources added

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,41 +14,17 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('ecommerce');
 });
 
-//To show all routes
-// Route::get('show-routes', function () {
-//     \Artison::call;
-// });
+Route::get('/slider', function () {
+    return view('slider');
+});
 
+Auth::routes();
 
+Route::get('/home', 'HomeController@index')->name('home');
 
-// Grouping different routes with prefix
-
-// Route::group(['Prefix'=>'blug'], function () {
-//     //different types of routes here
-// });
-
-
-
-
-//To call controller
-Route::get('first-routes','FirstController@myFunction');
-Route::get('/customer/{id?}','FirstController@Customers');
-Route::get('students','StudentController@myFunction');
-Route::get('students/detail','StudentController@studentFunction');
-Route::get('students/Post','PostController@studentPostFunction');
-
-
-
-//For to do applicaton
-Route::get('/todos','TodosController@index');
-Route::get('/edit_todo/{id}','TodosController@edit');
-Route::get('delete_todo/{id}','TodosController@delete');
-Route::get('/add_todo','TodosController@add');
-Route::get('/update/{id}','TodosController@update');
-Route::get('/save_new_todo','TodosController@save_new_todo');
 
 
 
